275d4dc9fce48e14f18ab457beb140a65848d8ac
João Vitor <joaovitorb3105@gmail.com>
2024-07-12 00:02:22 -0300

    fix: fixing filters

--- task-management/src/pages/home.tsx
+++ task-management/src/pages/home.tsx
@@ -1,67 +1,71 @@
 import { FaHome, FaPlus } from "react-icons/fa";
 import CardList from "../components/cardList";
-import { GiBiceps } from "react-icons/gi";
-import { MdTask, MdWork } from "react-icons/md";
-import { IoMdPerson } from "react-icons/io";
+import { MdTask } from "react-icons/md";
 import ResponsiveDatePickers from "../components/datePicker";
 import { Divider } from "@mui/material";
 import { tasksData } from "../mockData/data";
 import { useEffect, useState } from "react";
 import { ITask } from "../interfaces/task";
 import SideBarCard from "../components/sidebarCard";
-import dayjs, { Dayjs } from "dayjs";
+import { Dayjs } from "dayjs";
 import { formattedDate } from "../helper/formattedDate";
+import DialogComponent from "../components/dialog";
+import { BiPencil } from "react-icons/bi";
 
 export default function Home() {
   const [tasks, setTasks] = useState<ITask[]>([]);
-  const [filterDate, setFilterDate] = useState<string>(formattedDate(dayjs()));
+  const [filterDate, setFilterDate] = useState<string>("");
   const [selectedFilter, setSelectedFilter] = useState<string>("Home");
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [filters, setFilters] = useState<string[]>([]);
 
   useEffect(() => {
-    setTasks(tasksData);
-  }, []);
+    applyFilters();
+  }, [tasksData, filters, selectedFilter, filterDate]);
 
   function handleSidebarClick(data: string) {
     setSelectedFilter(data);
-    const filter = data.toLowerCase();
+  }
 
-    if (filter === "home") {
-      setTasks(tasksData);
-    } else if (filter === "completed") {
-      const filteredTasks = tasksData.filter((task) => task.status === true);
-      setTasks(filteredTasks);
+  function handleFilter(data: string) {
+    if (filters.includes(data)) {
+      const newFilters = filters.filter((filter) => filter !== data);
+      setFilters(newFilters);
     } else {
-      const filteredTasks = tasksData.filter(
-        (task) => task.type.toLowerCase() === filter,
-      );
-      setTasks(filteredTasks);
+      setFilters([...filters, data]);
     }
+  }
+
+  function applyFilters() {
+    let filteredTasks = tasksData.filter((task) => {
+      if (selectedFilter === "Home") {
+        return task.status === false;
+      } else if (selectedFilter === "Completed") {
+        return task.status === true;
+      } else {
+        return task.type.toLowerCase() === selectedFilter.toLowerCase();
+      }
+    });
+
+    if (filters.length > 0) {
+      filteredTasks = filteredTasks.filter((task) =>
+        filters.some((filter) => task.type.includes(filter)),
+      );
+    }
+
+    if (filterDate) {
+      filteredTasks = filteredTasks.filter((task) => task.date === filterDate);
+    }
+
+    setTasks(filteredTasks);
   }
 
   function handleDateChange(date: Dayjs | null) {
     if (date) {
       const newDate = formattedDate(date);
       setFilterDate(newDate);
-
-      if (selectedFilter === "Home") {
-        const filteredTasks = tasksData.filter((task) => task.date === newDate);
-        setTasks(filteredTasks);
-      } else if (selectedFilter === "Completed") {
-        const filteredTasks = tasksData.filter(
-          (task) => task.status === true && task.date === newDate,
-        );
-        setTasks(filteredTasks);
-      } else {
-        const filteredTasks = tasksData.filter(
-          (task) =>
-            task.type.toLowerCase() === selectedFilter.toLowerCase() &&
-            task.date === newDate,
-        );
-        setTasks(filteredTasks);
-      }
     } else {
-      setFilterDate(formattedDate(dayjs()));
-      setTasks(tasksData);
+      setFilterDate("");
     }
   }
 
@@ -84,25 +88,26 @@ export default function Home() {
             name="Completed"
             icon={MdTask}
           />
-          <SideBarCard
-            onClick={handleSidebarClick}
-            selected={selectedFilter === "Personal" ? true : false}
-            name="Personal"
-            icon={IoMdPerson}
-          />
-          <SideBarCard
-            onClick={handleSidebarClick}
-            selected={selectedFilter === "Work" ? true : false}
-            name="Work"
-            icon={MdWork}
-          />
-          <SideBarCard
-            onClick={handleSidebarClick}
-            selected={selectedFilter === "Diet" ? true : false}
-            name="Diet"
-            icon={GiBiceps}
-            deletable={true}
-          />
+
+          <div className="mt-2 flex flex-col gap-1">
+            <span className="font-semibold text-zinc-700">Filters</span>
+            <Divider
+              sx={{
+                background: "#27272a  ",
+                marginTop: "8px",
+                marginBottom: "8px",
+              }}
+            />
+
+            <SideBarCard
+              name="Personal"
+              checkable={true}
+              checked={handleFilter}
+            />
+            <SideBarCard name="Work" checkable={true} checked={handleFilter} />
+            <SideBarCard name="Diet" checkable={true} checked={handleFilter} />
+            <SideBarCard name="Study" checkable={true} checked={handleFilter} />
+          </div>
         </div>
 
         <div className="mt-4 flex w-full border-t-[1px] border-zinc-700">
@@ -113,11 +118,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex h-full flex-1 flex-col bg-zinc-900 px-14 py-8">
-        <h2 className="text-2xl font-semibold text-zinc-300">Todo List</h2>
-        <Divider sx={{ background: "#52525b ", marginTop: "4px" }} />
+      <div className="flex h-full flex-1 flex-col bg-zinc-900 px-24 py-8">
+        <h2 className="flex items-center justify-start gap-3 text-2xl font-semibold text-zinc-600">
+          Todo List <BiPencil />
+        </h2>
+        <Divider sx={{ background: "#3f3f46  ", marginTop: "4px" }} />
         <div className="mt-6 flex w-full items-center justify-start gap-3">
-          <button className="flex w-24 items-center gap-3 rounded bg-indigo-600 px-4 py-2 text-base font-semibold text-zinc-200 transition duration-150 ease-in-out hover:bg-indigo-700">
+          <button
+            onClick={() => setDialogOpen(true)}
+            className="flex w-24 items-center gap-3 rounded bg-indigo-600 px-4 py-2 text-base font-semibold text-zinc-200 shadow-sm shadow-zinc-950 transition duration-150 ease-in-out hover:bg-indigo-700"
+          >
             <FaPlus />
             Add
           </button>
@@ -138,6 +148,8 @@ export default function Home() {
           ))}
         </div>
       </div>
+
+      {dialogOpen && <DialogComponent onClose={() => setDialogOpen(false)} />}
     </section>
   );
 }
